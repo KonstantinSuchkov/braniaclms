f9c62601539eb0ccffdd2e2324c4c32399bf619f
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2022-10-27 13:30:39 +0300

    lesson 4 django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
+from mainapp.models import News
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
@@ -21,8 +22,11 @@ class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
 
     def get_context_data(self, **kwargs):
+        #context = super().get_context_data(**kwargs)
+        #context["news_title"] = "новостной заголовок для задания № 3"
+        #context["news_preview"] = "Описания новости с помощью цикла!"
+        #context["range"] = range(5)
+        #return context
         context = super().get_context_data(**kwargs)
-        context["news_title"] = "новостной заголовок для задания № 3"
-        context["news_preview"] = "Описания новости с помощью цикла!"
-        context["range"] = range(5)
+        context['object_list'] = News.objects.filter(deleted='False')
         return context
